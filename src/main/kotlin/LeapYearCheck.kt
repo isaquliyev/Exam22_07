79950256601a1ae3e5e78cd451a7dbcdb1555377
isaquliyev <isa.quluyev.wip@gmail.com>
2023-07-22 12:00:42 +0400

    second

--- src/main/kotlin/LeapYearCheck.kt
+++ src/main/kotlin/LeapYearCheck.kt
@@ -1,13 +1,16 @@
 fun leapYearChecker(number : Int) : Boolean {
     var checker = false
-    if (number % 4 == 0 ) {
+    if( number % 400 == 0 ) {
+        checker = true
+    }
+    else if (number % 4 == 0 && number % 100 != 0) {
         checker = true
     }
     return checker
 }
 
 fun main() {
-    val number : Int = 2020
+    val number : Int = 1000
     if (leapYearChecker(number)) {
         print("This year is leap year...")
     }
